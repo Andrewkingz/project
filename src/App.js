33e17077114b2b60006d1a4b9eaf9dbf39007e15
Andrewkingz <andrewboytie@gmail.com>
2022-02-14 21:28:10 +0100

    final

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Home from './Components/Home';
+import Sam from './Components/Sam';
+import Card from './Components/MediaCard';
+
+
+
+function App() {
+
+
+  return (
+    <div>
+     <Home/>
+     <Sam/>
+     <Card />
+      
+    </div>
+  );
+}
+
+export default App;
